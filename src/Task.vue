a64d3e39b9d473a1582886db1efb762d57607abb
teooko <teobogdann@yahoo.com>
2024-12-14 20:29:28 +0200

    Style tasks

--- src/Task.vue
+++ src/Task.vue
@@ -5,9 +5,22 @@
 </script>   
 
 <template>
-    <div>
+    <div class="task">
         {{ props.name }}
       <div v-if="props.done">DONE</div>
       <button @click="$emit('remove')">remove task</button>
     </div>
 </template>
+
+<style scoped>
+    .task {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        width: 500px;
+    }
+</style>
